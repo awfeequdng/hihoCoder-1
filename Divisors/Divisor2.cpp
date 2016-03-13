1825297d3610eb131adf9ad66bd4aa7d3c35ffd5
zuilehongdou <15120101@bjtu.edu.cn>
2016-03-13 15:09:25 +0800

    my method so slow

---
+++ Divisors/Divisor2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int count(int a)
+{
+    int temp = 0;
+    for(int i = 1; i <= a; ++i)
+    {
+        if(a % i == 0)
+            temp++;
+    }
+    return temp;
+}
+int main()
+{
+    int n;
+  	cin >> n;
+
+    int temp = 0;
+    int res = 0;
+    for(int i = 1; i <= n; ++i)
+    {
+       if(count(i) > temp)
+        {
+            temp = count(i);
+            res = i;
+        }
+    }
+    cout << res << endl;
+    return 0;
+}

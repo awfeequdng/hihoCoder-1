81c2655e4398e232eee950c1134729e6b34f81dc
zuilehongdou <15120101@bjtu.edu.cn>
2016-03-13 12:39:41 +0800

    add files

---
+++ Divisors/Divisor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int count(int a)
+{
+    int temp = 0;
+    for(int i = 1; i <= a; ++i)
+    {
+        if(a % i == 0)
+            temp++;
+    }
+    return temp;
+}
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        int temp = 0;
+        int res = 0;
+        for(int i = 1; i <= n; ++i)
+        {
+            if(count(i) > temp)
+            {
+                temp = count(i);
+                res = i;
+            }
+        }
+        cout << res << endl;
+    }
+    return 0;
+}

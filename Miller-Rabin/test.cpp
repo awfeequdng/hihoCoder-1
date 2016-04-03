844c451f89d11f510806591646911f70c4928114
zuilehongdou <15120101@bjtu.edu.cn>
2016-04-03 18:04:51 +0800

    add files

--- Miller-Rabin/test.cpp
+++ Miller-Rabin/test.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <vector>
 #include <math.h>
 using namespace std;
@@ -11,4 +11,31 @@ int main()
 	for(const auto x : num)
 		cout << pow(x, 2) << endl;
 	return 0;
-}
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 1;
+	int num = 8;
+	while(num--)
+	{
+	a = a << 1;
+	cout << a << endl;
+	}
+	return 0;
+}*/
+
+	#include <iostream>
+	using namespace std;
+	int main()
+	{
+		int num = 10;
+		while(num--)
+		{
+			cout << num << ":" << (num & 1) << endl;
+		}
+		return 0;
+
+	}

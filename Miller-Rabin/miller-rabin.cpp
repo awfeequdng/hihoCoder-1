2f8109e9a3443c118ada0f435e72f71d20279dfc
zuilehongdou <15120101@bjtu.edu.cn>
2016-04-05 09:50:17 +0800

    add files

--- Miller-Rabin/miller-rabin.cpp
+++ Miller-Rabin/miller-rabin.cpp
@@ -3,6 +3,80 @@
 #include <stdlib.h>
 #include <math.h>
 using namespace std;
+int get_mod(int a, int b, int c)
+{
+    long long res = 1;
+    long long temp = a;
+    while(b > 0)
+    {
+        if( b & 1)
+        {
+            res = (res * temp) % c;
+        }
+        temp = (temp * temp) % c;
+        b >>= 1;
+    }
+    return static_cast<int> (res);
+}
+bool Miller_Rabin(int n)
+{
+    if (n < 2) 
+        return false;
+    if(n == 2 || n == 3)
+        return true;
+
+    long long u = n - 1;
+    while(u % 2 == 0)
+        u = u / 2;
+    for(int i = 0; i < 5; ++i)
+    {
+        int a = rand() % (n - 2) + 2;
+        int y = get_mod(a, n-1, n);
+        if(y != 1)
+            return false;
+        int x = get_mod(a, u, n);
+        if(x == 1 || x == n-1)
+            continue;
+        for(int i = u; i < n; i *= 2)
+        {
+            x = x * x % n;
+            if(x == 1)
+                return false;
+            if(x == n-1)
+            {
+                break;
+                continue;
+            }
+            return false;
+
+        }
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums;
+    int temp;
+    while(n--)
+    {
+        cin >> temp;
+        nums.push_back(temp);
+    }
+    for(const auto num : nums)
+    {
+        if(Miller_Rabin(num))
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+}*/
+/*#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
 typedef long long  llong;
 llong get_mod(llong a, llong b, llong c)
 {
@@ -47,7 +121,7 @@ int main()
     return 0;
 }*/
 
-
+/*
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
@@ -113,4 +187,4 @@ int main()
         else
             cout << "No" << endl;
     }
-}
+}*/

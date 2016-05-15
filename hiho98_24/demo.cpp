0ed200bedb2453aec12edf34e633e503ab132826
zuilehongdou <15120101@bjtu.edu.cn>
2016-05-15 12:36:16 +0800

    update

--- hiho98_24/demo.cpp
+++ hiho98_24/demo.cpp
@@ -6,31 +6,40 @@
 #include <vector>
 using namespace std;
 
-//1 +; 2 -; 3 *; 4 /; 5 反-; 6 反/
-bool used[4] = false;
-int nowNumber[] = {0, 0, 0, 0};
+//0 +; 1 -; 2 *; 3 /; 4 反-; 5 反/
+bool used[4] = {false};
+int nowNumber[4] = {0, 0, 0, 0};
 int ops[3] = {0, 0, 0};
+vector<int> number;
+
+bool makeOperation(int depth);
 
 int cal(int a, int b, int opt)
 {
     switch(opt)
     {
-        case 1:
+        case 0:
             return a + b;
-        case 2:
+            break;
+        case 1:
             return a - b;
-        case 3:
+            break;
+        case 2:
             return a * b;
-        case 4:
+            break;
+        case 3:
             return a / b;
-        case 5:
+            break;
+        case 4:
             return b - a;
-        case 6:
+            break;
+        case 5:
             return b / a;
+            break;
     }
 }
 
-int calcType1(int *n. int *ops)
+int calcType1(int *n, int *ops)
 {
     return cal(cal(cal(n[0], n[1], ops[0]), n[2], ops[1]), n[3], ops[2]);
 }
@@ -48,7 +57,7 @@ bool makeNumber(int depth)
     {
         if(!used[i])
         {
-            nowNumber[i] = number[i];
+            nowNumber[depth] = number[i];
             used[i] = true;
             if(makeNumber(depth + 1))
                 return true;
@@ -80,18 +89,18 @@ bool makeOperation(int depth)
 int main()
 {
     int num, temp;
-    vector<int> number;
     cin >> num;
-    for(int i = 0; i < num; ++i)
+    while(num--)
     {
-        cin >> temp;
-        number.push_back(temp);
-    }
-
+        for(int i = 0; i < 4; ++i)
+        {
+            cin >> temp;
+            number.push_back(temp);
+        }
     if(makeNumber(0))
         cout << "Yes" << endl;
     else
         cout << "No" << endl;
-
+    }
     return 0;
 }

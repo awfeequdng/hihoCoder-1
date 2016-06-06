a5791b1f8b68ed23a00899609d42ed2cc9be530f
haoyuanliu <15120101@bjtu.edu.cn>
2016-06-06 22:06:33 +0800

    update

--- hiho100_启发式搜索/find.cpp
+++ hiho100_启发式搜索/find.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <math.h>
 using namespace std;
 
-int fact(int t)
+const int number[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+const int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+struct state //状态类
+{
+    int val;
+    int f, g, h;
+    state(int val_, int g_, int h_) : val(val_), g(g_), h(h_)
+    {
+        f = g + h;
+    }
+
+    friend bool operator < (const state &p, const state &q) //设定比较函数
+    {
+        return p.f > q.f;
+    }
+};
+
+int distance(int p[3][3]) //计算曼哈顿距离
+{
+    int res = 0;
+    for(int i = 0; i < 3; ++i)
+    {
+        for(int j = 0; j < 3; ++j)
+        {
+            if(p[i][j] == 9)
+                continue;
+            int x = (p[i][j] - 1) / 3;
+            int y = (p[i][j] - 1) % 3;
+
+            res += abs(x - i) + abs(y - j);
+        }
+    }
+    return res;
+}
+
+int fact(int t) //阶乘函数
 {
     int res = 1;
     for(int i = 1; i <= t; ++i)
@@ -36,7 +76,96 @@ int contor(int val) //康托展开
 
 int uncontor(int X) //逆康托展开
 {
-    
+   int a[9];
+   int v = X;
+   for(int i = 0; i < 9; ++i)
+   {
+        a[i] = v / fact(8 - i);
+        v %= fact(8 - i);
+   }
+
+   int val = 0;
+   list<int> q(number, number + 9);
+   for(int i = 0; i < 9; ++i)
+   {
+       list<int>::iterator p = q.begin();
+       while(a[i]-- > 0)
+           p++;
+       val = val * 10 + *p;
+       q.erase(p);
+   }
+
+   return val;
+}
+
+int bfs(int start)
+{
+    int board[3][3];
+    int v = start;
+    for(int i = 2; i >= 0; --i)
+    {
+        for(int j = 2; j >= 0; --j)
+        {
+            board[i][j] = v % 10;
+            v /= 10;
+        }
+    }
+
+    priority_queue<state> open;
+    vector<int> close(fact(9), -1);
+
+    open.push(state(start, 0, distance(board)));
+
+    while(!open.empty() && close[0] == -1)
+    {
+        state s = open.top();
+        open.pop();
+
+        int k = contor(s.val);
+        if(close[k] != -1)
+            continue;
+        close[k] = s.g;
+
+        int v = s.val;
+        for(int i = 2; i >= 0; --i)
+        {
+            for(int j = 2; j >= 0; --j)
+            {
+                board[i][j] = v % 10;
+                v /= 10;
+            }
+        }
+
+        for(int i = 0; i < 3; ++i)
+        {
+            for(int j = 0; j < 3; ++j)
+            {
+                if(board[i][j] == 9)
+                {
+                    for(int t = 0; t < 4; ++t)
+                    {
+                        int x = i + dir[t][0];
+                        int y = j + dir[t][1];
+                        if(x >= 0 && x < 3 && y >= 0 && y < 3)
+                        {
+                            swap(board[i][j], board[x][y]);
+                            int u = 0;
+                            for(int p = 0; p < 3; ++p)
+                            {
+                                for(int q = 0; q < 3; ++q)
+                                {
+                                    u = u * 10 + board[p][q];
+                                }
+                            }
+                            open.push(state(u, s.g + 1, distance(board)));
+                            swap(board[i][j], board[x][y]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return close[0];
 }
 
 int main()
@@ -45,7 +174,6 @@ int main()
     int num[9];
     int n;
     cin >> n;
-    cout << fact(n) << endl;
     while(n--)
     {
         int val = 0;
@@ -57,6 +185,11 @@ int main()
             num[i] = temp;
             val = val * 10 + temp;
         }
+        int c = bfs(val);
+        if(c == -1)
+            cout << "No Solution!\n";
+        else
+            cout << c << endl;
     }
     return 0;
 }

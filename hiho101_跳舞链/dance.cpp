65b7d3325efcf4c2c93ad4e847710f17dbbf2b8a
zuilehongdou <15120101@bjtu.edu.cn>
2016-06-08 16:37:43 +0800

    add files

--- hiho101_跳舞链/dance.cpp
+++ hiho101_跳舞链/dance.cpp
@@ -16,7 +16,7 @@ struct node//node结构
     node(node *left_, node *right_, node *up_, node *down_, int x_, int y_) : left(left_), right(right_), up(up_), down(down_), x(x_), y(y_) {}
 };
 
-void build(int board[n][m])
+void build(vector<pair<int, int>> s)
 {
     node *head;
     *head = node(head, head, head, head, 0, 0);
@@ -38,21 +38,6 @@ void build(int board[n][m])
         pre->right = p;
         pre = p;
     }
-    int count = 0;
-    int id[n][m];
-    vector<node> N;
-    for(int i = 0; i < n; ++i)
-    {
-        for(int j = 0; j < m; ++j)
-        {
-            if(board[i][j] == 1)
-            {
-                count++;
-                id[i][j] = count;
-                N.push_back(node(N[count], N[count], N[count], N[count], i, j));
-            }
-        }
-    }
 }
 
 int main()
@@ -62,6 +47,7 @@ int main()
     cin >> t;
     while(t--)
     {
+        vector<pair<int, int>> state;
         cin >> n >> m;
         int num[n][m];
         for(int i = 0; i < n; ++i)
@@ -72,7 +58,6 @@ int main()
                 num[i][j] = temp;
             }
         }
-        build(num);
     }
     return 0;
 }

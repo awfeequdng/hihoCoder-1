694e540f709362717feeec7b23941e2dc79bf34b
haoyuanliu <15120101@bjtu.edu.cn>
2016-06-09 14:07:08 +0800

    update

--- hiho101_跳舞链/test/shared_ptr.cpp
+++ hiho101_跳舞链/test/shared_ptr.cpp
@@ -9,5 +9,9 @@ int main()
     cout << *p1 << endl;
     shared_ptr<int> p2 = make_shared<int>(22);
     cout << *p2 << endl;
+    shared_ptr<string> p3 = make_shared<string>(10, '9');
+    cout << *p3 << endl;
+    shared_ptr<int> p4 = make_shared<int>();
+    cout << *p4 << endl;
     return 0;
 }

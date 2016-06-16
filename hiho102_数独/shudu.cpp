f70da5940db561395ac6e18f7fba8d264271ea0c
zuilehongdou <15120101@bjtu.edu.cn>
2016-06-16 22:44:43 +0800

    update

--- hiho102_数独/shudu.cpp
+++ hiho102_数独/shudu.cpp
@@ -128,7 +128,7 @@ void build()
     //横向添加结点
     for(int i = 1; i <=n; ++i)
     {
-        pre = NULL;
+        Node *pre = NULL; //???
         for(int j = 1; j <= m; ++j)
         {
             if(matrix[i][j] == 1)

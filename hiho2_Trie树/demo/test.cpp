1e2a90ce97769cec6b1510a94bc7bfaa5f8807c9
zuilehongdou <15120101@bjtu.edu.cn>
2016-06-17 17:36:06 +0800

    update

--- hiho2_Trie树/demo/test.cpp
+++ hiho2_Trie树/demo/test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     int test[10];
-    memset(test, -1, sizeof(test));
+    memset(test, -2, sizeof(test));
     for(const auto x : test)
         cout << x << endl;
     return 0;

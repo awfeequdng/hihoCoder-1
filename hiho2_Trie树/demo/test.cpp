2edbd45b0eca14408a69e90a575dd8d5bd350c8b
zuilehongdou <15120101@bjtu.edu.cn>
2016-06-21 09:06:46 +0800

    update

--- hiho2_Trie树/demo/test.cpp
+++ hiho2_Trie树/demo/test.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int main()
 {
+    string str;
+    while(cin >> str)
+    {
+        cout << str << endl;
+        cout << str.size() << endl;
+    }
     int test[10];
     memset(test, -2, sizeof(test));
     for(const auto x : test)

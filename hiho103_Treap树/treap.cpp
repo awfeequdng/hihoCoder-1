981b93c482d3aa655a7c8e257f5bbfc9ff9c2cd3
zuilehongdou <15120101@bjtu.edu.cn>
2016-06-26 16:53:12 +0800

    update

--- hiho103_Treap树/treap.cpp
+++ hiho103_Treap树/treap.cpp
@@ -6,15 +6,107 @@ typedef struct node
 {
     int key;
     int weight;
-    struct node *fa, *lchild, *rchild;
+    struct node *father, *left, *right;
     node() {}
-    node(int key_, int weight_) : key(key_), weight(weight_) {}
+    node(int key_, int weight_) : key(key_), weight(weight_) {this->left = NULL; this->right = NULL;}
 }*Node;
+
+//左旋函数
+void left_rotate(Node a)
+{
+    Node b = a->right;
+    b->father = a->father;
+    if(a->father->left == a)
+        a->father->left = b;
+    else
+        a->father->right = b;
+    b->left->father = a;
+    a->right = b->left;
+    a->father = b;
+    b->left = a;
+}
+
+//右旋函数
+void right_rotate(Node a)
+{
+    Node b = a->left;
+    b->father = a->father;
+    if(a->father->left == a)
+        a->father->left = b;
+    else
+        a->father->right = b;
+    b->right->father = a;
+    a->left = b->right;
+    a->father = b;
+    b->right = a;
+}
+
+//插入结点
+Node insert(Node n, int key)
+{
+    if(key < n->key)
+    {
+        if(n->left == NULL)
+        {
+            n->left = new node(key, random());
+            return n->left;
+        }
+        else
+            return insert(n->left, key);
+    }
+    else
+    {
+        if(n->right == NULL)
+        {
+            n->right = new node(key, random());
+            return n->right;
+        }
+        else
+            return insert(n->right, key);
+    }
+}
+
+//旋转结点
+void rotate(Node node)
+{
+    while(node->father)
+    {
+        Node fa = node->father;
+        if(node->weight < fa->weight)
+        {
+            if(node == fa->left)
+                right_rotate(node);
+            else
+                left_rotate(node);
+        }
+        else
+            break;
+    }
+}
+
 
 int main()
 {
-    Node root = new node;
-    *root = node(1, 2);
-    cout << root->key << " " << root->weight << endl;
+    Node root = NULL;
+    int n;
+    int num;
+    char ch;
+    cin >> num;
+    while(n--)
+    {
+        cin >> ch;
+        cin >> num;
+        if(ch == 'I')
+        {
+            if(root == NULL)
+                root = new node(num, random());
+            else
+                rotate(insert(root, x));
+        }
+        elseif(ch == 'Q')
+        {
+            query(num);
+        }
+    }
     return 0;
 }

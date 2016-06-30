05761533734f4324771c8d51788fee6322208b79
zuilehongdou <15120101@bjtu.edu.cn>
2016-06-30 18:12:03 +0800

    update

--- hiho4_Tries图/demo/demo.cpp
+++ hiho4_Tries图/demo/demo.cpp
@@ -2,8 +2,8 @@
 #include<string>
 #include<queue>
 using namespace std;
-//结点结构 
-class Node{ 
+//结点结构
+class Node{
 	public:
 	Node()
 	{
@@ -16,18 +16,17 @@ class Node{
 	}
 	bool bad;//是否是终点
 	Node *next[26] ;//26个字母
-	Node *prev;//前缀结点 
+	Node *prev;//前缀结点
 };
 class TrieTree{
 	public:
 	TrieTree()
 	{
 		root=new Node();
-	} 
+	}
 	void insert(string &word);
-	void build();//创建Trie图 
+	void build();//创建Trie图
 	bool search(string &word);
-	
 	private:
 	Node *root;
 };
@@ -38,27 +37,27 @@ void TrieTree::insert(string &word)
 	{
 		if(proot->next[word.at(i)-'a']==NULL)
 			proot->next[word.at(i)-'a']=new Node();
-		proot=proot->next[word.at(i)-'a'];			
+		proot=proot->next[word.at(i)-'a'];
 	}
-	proot->bad=true;//单词结尾结点 
+	proot->bad=true;//单词结尾结点
 }
 void TrieTree::build()
 {
 	queue<Node*> Q;
 	Node *proot=root;
-	//root->prev=root;//根节点的前缀还是根节点 
-	for(int i=0;i<26;i++)//初始化第一层结点 
+	//root->prev=root;//根节点的前缀还是根节点
+	for(int i=0;i<26;i++)//初始化第一层结点
 	{
 		if(proot->next[i]!=NULL)
 		{
-			proot->next[i]->prev=root;//第一层结点的前缀是根节点 
-			Q.push(proot->next[i]);//第一层结点入队 
+			proot->next[i]->prev=root;//第一层结点的前缀是根节点
+			Q.push(proot->next[i]);//第一层结点入队
 		}
 	}
 	while(!Q.empty())
 	{
-		proot=Q.front();//返回第一个元素 
-		Q.pop();//删除第一个元素 
+		proot=Q.front();//返回第一个元素
+		Q.pop();//删除第一个元素
 		for(int i=0; i<26; i++)
 		{
 			Node *p=proot->next[i];
@@ -69,7 +68,7 @@ void TrieTree::build()
 				{
 					if(prev->next[i]!=NULL)
 					{
-						p->prev=prev->next[i];//设置孩子结点的前缀结点 
+						p->prev=prev->next[i];//设置孩子结点的前缀结点
 						if(p->prev->bad==true)
 							p->bad=true;
 						break;
@@ -77,16 +76,16 @@ void TrieTree::build()
 					else
 						prev=prev->prev;
 				}
-				
-				if(p->prev==NULL)//如果没有前缀，最后指向根节点 
-					p->prev=root; 				
-				
-				//if(p->bad==false)//如果p结点为坏结点，它的孩子其实就没必要判断了 
-				Q.push(p);//队列中加入p 
+
+				if(p->prev==NULL)//如果没有前缀，最后指向根节点
+					p->prev=root;
+
+				//if(p->bad==false)//如果p结点为坏结点，它的孩子其实就没必要判断了
+				Q.push(p);//队列中加入p
 			} //end if
 		} //end for
-	}//end while	
-} 
+	}//end while
+}
 
 bool TrieTree::search(string &word)
 {
@@ -108,11 +107,11 @@ bool TrieTree::search(string &word)
 			if(proot==root||proot==NULL)
 			{
 				proot=root;
-				break; 
+				break;
 			}
-				
+
 		}
-		
+
 	}
 	return false;
 }
@@ -121,8 +120,8 @@ int main()
 {
 	int N;
 	TrieTree T;
-	string s;//过滤单词 
-	string sen;//文章 
+	string s;//过滤单词
+	string sen;//文章
 	cin>>N;
 	for(int i=0; i<N; i++)
 	{
@@ -134,7 +133,7 @@ int main()
 	bool test=T.search(sen);
 	if(test)
 		cout<<"YES"<<endl;
-	else 
+	else
 		cout<<"NO"<<endl;
 	return 0;
 }

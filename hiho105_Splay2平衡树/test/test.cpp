a39f29565a2c425abfd9151b51fb1365d605c4ba
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-04 14:39:26 +0800

    update

---
+++ hiho105_Splay2平衡树/test/test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+typedef struct node
+{
+    int key;
+    int id;
+    bool color;
+    node *parent, *leftchild, *rightchild;
+    node(){}
+    node(int id_, int key_) : id(id_), key(key_)
+    {
+        parent = NULL;
+        leftchild = NULL;
+        rightchild = NULL;
+    }
+    bool operator < (const node *a)
+    {
+        return this->id < a->id;
+    }
+}*Node;
+
+int main()
+{
+    set<Node> s;
+    int id, key;
+    for(int i = 0; i < 5; ++i)
+    {
+        cin >> id >> key;
+        s.insert(new node(id, key));
+    }
+    for(const auto x : s)
+    {
+        cout << x->id << ' ' << x->key << endl;
+    }
+    return 0;
+}

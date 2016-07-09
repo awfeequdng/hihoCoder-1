ab53203a7425a8d63166cf73f1bec56557074738
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-09 21:03:28 +0800

    update

--- hiho6_背包问题/demo.cpp
+++ hiho6_背包问题/demo.cpp
@@ -3,33 +3,36 @@
 #define MAX 100001
 using namespace std;
 int res[MAX];
+
+int max(int x, int y)
+{
+    if(x > y)
+        return x;
+    else
+        return y;
+}
+
 int main()
 {
     vector<pair<int, int>> vec;
     int need, value, self;
     int n, m;
     cin >> n >> m;
-    while(n--)
+    for(int i = 0; i < n; ++i)
     {
         cin >> need >> value;
         vec.push_back(make_pair(need, value));
     }
-    for(const auto x : vec)
-    {
-        cout << x.first << " " << x.second << endl;
-    }
     for(int i = 0; i < MAX; ++i)
         res[i] = 0;
-    for(int i = 1; i < n; ++i)
+    for(int i = 0; i < n; ++i)
     {
-        for(int j = m; j <= vec[i].first; --j)
+        for(int j = m; j >= vec[i].first; --j)
         {
             self = res[j];
             res[j] = max(self, res[j - vec[i].first] + vec[i].second);
         }
     }
-    for(int i = 0; i < m; ++i)
-        cout << res[i] << " ";
-    cout << endl;
+    cout << res[m] << endl;
     return 0;
 }

ea51ec1c82a47ddf554a135e971a7c9434323c59
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-12 16:38:17 +0800

    add files

--- hiho106_SBT树/sbt.cpp
+++ hiho106_SBT树/sbt.cpp
@@ -6,13 +6,6 @@ typedef struct node
     int key;
     int num;
     node *father, *left, *right;
-    node()
-    {
-        num = 1;
-        father = NULL;
-        left = NULL;
-        right = NULL;
-    }
     node(int key_) : key(key_)
     {
         num = 1;
@@ -24,143 +17,182 @@ typedef struct node
 
 Node root = NULL;
 
-void right_rotate(Node &t)
+void right_rotate(Node &n)
 {
-    Node k = t->left;
-    t->left = k->right;
-    k->right = t;
-    k->num = t->num;
-    t->num = t->left->num + t->right->num + 1;
-    t = k;
+    if(n == NULL)
+        return;
+    Node k = n->left;
+    if(k)
+    {
+        Node p = n->father;
+        k->father = n->father;
+        if(p)
+        {
+            if(p->left == n)
+                p->left = k;
+            else
+                p->right = k;
+        }
+        else
+            k = root;
+
+        n->left = k->right;
+        if(k->right)
+            k->right->father = n;
+
+        k->right = n;
+        n->father = k;
+
+        k->num = n->num;
+
+        if(n->right && n->left)
+            n->num = n->right->num + n->left->num + 1;
+        else if(n->right)
+            n->num = n->right->num + 1;
+        else if(n->left)
+            n->num = n->left->num + 1;
+        else
+            n->num = 1;
+
+        n = k;
+    }
 }
 
-void left_rotate(Node &t)
+void left_rotate(Node &n)
 {
-    Node k = t->right;
-    t->right = k->left;
-    k->left = t;
-    k->num =t->num;
-    t->num = t->left->num + t->right->num + 1;
-    t = k;
+    if(n == NULL)
+        return;
+    Node k = n->right;
+    if(k)
+    {
+        Node p = n->father;
+        if(p)
+        {
+            if(p->left == n)
+                p->left = k;
+            else
+                p->right = k;
+        }
+        else
+            root = k;
+
+        n->right = k->left;
+        if(k->left)
+            k->left->father = n;
+
+        k->left = n;
+        n->father = k;
+
+        k->num = n->num;
+
+        if(n->left && n->right)
+            n->num = n->left->num + n->right->num + 1;
+        else if(n->left)
+            n->num = n->left->num + 1;
+        else if(n->right)
+            n->num = n->right->num + 1;
+        else
+            n->num = 1;
+
+        n = k;
+    }
 }
 
-Node maintain(Node t, bool flag)
+void maintain(Node n, bool flag)
 {
+    if(n == NULL)
+        return;
     if(!flag)
     {
-        if(t->left->left->num > t->right->num)
-            right_rotate(t);
-        else if(t->left->right->num > t->right->num)
+        if(n->left)
         {
-            left_rotate(t->left);
-            right_rotate(t);
+            if(n->left->left && (!n->right || n->left->left->num > n->right->num))
+                right_rotate(n);
+            else if(n->left->right && (!n->right || n->left->right->num > n->right->num))
+            {
+                left_rotate(n->left);
+                right_rotate(n);
+            }
         }
         else
-            return t;
+            return;
     }
     else
     {
-        if(t->right->right->num > t->left->num)
-            left_rotate(t);
-        else if(t->right->left->num > t->left->num)
+        if(n->right)
         {
-            right_rotate(t->right);
-            left_rotate(t);
+            if(n->right->right && (!n->left || n->right->right->num > n->left->num))
+                left_rotate(n);
+            else if(n->right->left && (!n->left || n->right->left->num > n->left->num))
+            {
+                right_rotate(n->right);
+                left_rotate(n);
+            }
         }
         else
-            return t;
+            return;
     }
-    maintain(t->left, false);
-    maintain(t->right, true);
-    maintain(t, false);
-    maintain(t, true);
+    maintain(n->left, false);
+    maintain(n->right, true);
+    maintain(n, false);
+    maintain(n, true);
 }
 
-Node bst_insert(Node n, int key)
-{
-    if(key < n->key)
-    {
-        if(n->left == NULL)
-        {
-            n->left = new node(key);
-            n->left->father = n;
-        }
-        else
-            return bst_insert(n->left, key);
-    }
-    else
-    {
-        if(n->right == NULL)
-        {
-            n->right = new node(key);
-            return n->right;
-        }
-        else
-            return bst_insert(n->right, key);
-    }
-}
-void insert(int key)
+Node insert(Node n, int key)
 {
     if(root == NULL)
+    {
         root = new node(key);
+        return root;
+    }
     else
     {
-        Node node = bst_insert(root, key);
-        maintain(root, key >= root->key);
-    }
-}
-
-int del(Node t, int key)
-{
-    int ret = 0;
-    t->num -= 1;
-    if(key == t->key || (key < t->key && t->left == NULL) || (key > t->key && t->right == NULL))
-    {
-        ret = t->key;
-        if(t->left == NULL || t->right == NULL)
+        if(n->key > key)
         {
-            if(t->left)
-                t = t->left;
+            if(n->left == NULL)
+            {
+                n->left = new node(key);
+                n->left->father = n;
+                return n->left;
+            }
             else
-                t = t->right;
+                return insert(n->left, key);
         }
         else
-            t->key = del(t->left, t->key + 1);
-    }
-    else
-    {
-        if(key < t->key)
-            return del(t->left, key);
-        else
-            return del(t->right, key);
-    }
-    return ret;
-}
-
-Node bst_find(Node node, int key)
-{
-    if(key == node->key)
-        return node;
-    if(key < node->key)
-    {
-        if(node->left == NULL)
-            return NULL;
-        else
-            return bst_find(node->left, key);
-    }
-    else
-    {
-        if(node->right == NULL)
-            return NULL;
-        else
-            return bst_find(node->right, key);
+        {
+            if(n->right == NULL)
+            {
+                n->right = new node(key);
+                n->right->father = n;
+                return n->right;
+            }
+            else
+                return insert(n->right, key);
+        }
+        maintain(n, key >= n->key);
     }
 }
 
-void query(int key)
+void query(Node p, int key)
 {
-    Node node = bst_find(root, key);
-    cout << node->num << endl;
+    if(p->num < key)
+        return;
+    if(p->left)
+    {
+        if(p->left->num == key - 1)
+            cout << p->key << endl;
+        else if(p->left->num < key - 1)
+            query(p->right, key - p->left->num - 1);
+        else
+            query(p->left, key);
+    }
+    else
+    {
+        if(key == 1)
+            cout << p->key << endl;
+        else
+            query(p->right, key - 1);
+    }
 }
 
 int main()
@@ -173,12 +205,10 @@ int main()
         cin >> ch >> num;
         if(ch == 'I')
         {
-            insert(num);
-            if(!root)
-                cout << "Root is NULL" << endl;
+            insert(root, num);
         }
         else if(ch == 'Q')
-            query(num);
+            query(root, num);
     }
     return 0;
 }

479f0af90a550dfee4ada494f8e6665b843f02c7
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-14 09:14:19 +0800

    add final version

--- hiho8_状态压缩/byte/test.cpp
+++ hiho8_状态压缩/byte/test.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+    int temp = 16;
+    while(temp > 0)
+    {
+        cout << (temp >> 1) << endl;
+        temp = temp >> 1;
+    }
     for(int i = 10; i < 20; ++i)
     {
         cout << ((i & 0x7)>>1) << endl;
--- hiho8_状态压缩/demo.cpp
+++ hiho8_状态压缩/demo.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 using namespace std;
-
+int ref[] = {0, 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023};
+int count(int n)
+{
+    int res = 0;
+    while(n > 0)
+    {
+        n = n >> 1;
+        res++;
+    }
+    return res;
+}
+int main()
+{
+    int n, m, q, temp;
+    cin >> n >> m >> q;
+    int state = 0;
+    int res = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> temp;
+        state = (((state << 1) + 1) & ref[m]);
+        if(count(state) <= q)
+            res += temp;
+        else
+            state -= 1;
+    }
+    cout << res << endl;
+    return 0;
+}

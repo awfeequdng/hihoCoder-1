9b63f46e37cae2ef752779bd60eaffda8f735d3a
haoyuanliu <15120101@bjtu.edu.cn>
2016-07-14 23:19:23 +0800

    update

---
+++ hiho9_状态压缩_二/demo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int b[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+int sum[2][5][1024] = {1};
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; ++i)
+    {
+        for(int j = 0; j < b[m]; ++j)
+        {
+            for(int k = 0; k < b[2 * m]; ++k)
+            {
+            }
+        }
+    }
+}

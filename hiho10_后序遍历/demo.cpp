db35c3657beafd13d9263377ec48a0b6f44f421a
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-17 22:50:58 +0800

    update

--- hiho10_后序遍历/demo.cpp
+++ hiho10_后序遍历/demo.cpp
@@ -9,21 +9,21 @@ string postOrder(string str1, string str2)
     root = str1.substr(0, 1);
     int lenLeft = str2.find(root[0], 0);
     int lenRight = str1.size() - 1 - lenLeft;
-    string preLeft = str1.substr(1, lenLeft);
-    string preRight = str1.substr(lenLeft+1);
-    string midLeft = str2.substr(0, lenLeft);
-    string midRight = str2.substr(lenLeft+1);
+    //string preLeft = str1.substr(1, lenLeft);
+    //string preRight = str1.substr(lenLeft+1);
+    //string midLeft = str2.substr(0, lenLeft);
+    //string midRight = str2.substr(lenLeft+1);
     if(lenLeft == 1 || lenRight == 1)
     {
         if(lenLeft == 1)
-            left = midLeft;
+            left = str2.substr(0, lenLeft);
         if(lenRight == 1)
-            right = midRight;
+            right = str2.substr(lenLeft+1);
     }
     else
     {
-        left = postOrder(preLeft, midRight);
-        right = postOrder(preRight, midRight);
+        left = postOrder(str1.substr(1, lenLeft), str2.substr(0, lenLeft));
+        right = postOrder(str1.substr(lenLeft+1), str2.substr(lenLeft+1));
     }
     return res = left + right + root;
 }

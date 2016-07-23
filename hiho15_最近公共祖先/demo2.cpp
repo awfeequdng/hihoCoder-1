c032f63d52c63b4c041d490a0e13d47d489c1520
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-23 22:25:22 +0800

    update

--- hiho15_最近公共祖先/demo2.cpp
+++ hiho15_最近公共祖先/demo2.cpp
@@ -2,27 +2,26 @@
 #include <map>
 #include <vector>
 #include <string>
-using namespace std;
 #define N 100010
+using namespace std;
 
 typedef struct node
 {
     string name;
-    node* father;
     int color;
+    node* father;
     vector<node*> child;
     node(string name_) : name(name_)
     {
-        father = NULL;
         color = 0;
+        father = NULL;
     }
 }*Node;
 
 string res[N];
 map<string, Node> tree;
-map<string, Node>::iterator it;
-typedef pair<Node, int> NodeNum;
-map<string, vector<NodeNum>> query;
+typedef pair<Node, int> nodeNum;
+map<string, vector<nodeNum>> query;
 
 Node findRoot(Node p)
 {
@@ -38,28 +37,29 @@ Node findRoot(Node p)
 void dfs(Node p)
 {
     p->color = 1;
-    vector<NodeNum> &pChild = query[p->name];
-    for(int i = 0; i < pChild.size(); ++i)
+    vector<nodeNum> &pQuery = query[p->name];
+    for(int i = 0; i < pQuery.size(); ++i)
     {
-        Node pQuery = pChild[i].first;
-        int id = pChild[i].second;
-        if(pQuery->color == 0)
+        Node pChild = pQuery[i].first;
+        int id = pQuery[i].second;
+        if(pChild->color == 0)
             continue;
-        res[id] = findRoot(pQuery)->name;
+        res[id] = findRoot(pChild)->name;
     }
 
     for(int i = 0; i < p->child.size(); ++i)
     {
         dfs(p->child[i]);
     }
+
     p->color = 2;
 }
 
 int main()
 {
-    Node root = NULL;
-    string s1, s2;
     int n, m;
+    string s1, s2;
+    Node root = NULL;
     cin >> n;
     for(int i = 0; i < n; ++i)
     {
@@ -70,7 +70,8 @@ int main()
             tree[s2] = new node(s2);
         tree[s1]->child.push_back(tree[s2]);
         tree[s2]->father = tree[s1];
-        if(i ==0)
+
+        if(i == 0)
             root = tree[s1];
     }
 
@@ -79,12 +80,13 @@ int main()
     {
         cin >> s1 >> s2;
         if(query.find(s1) == query.end())
-            query[s1] = vector<NodeNum>();
+            query[s1] = vector<nodeNum>();
         if(query.find(s2) == query.end())
-            query[s2] = vector<NodeNum>();
+            query[s2] == vector<nodeNum>();
         Node p1 = tree[s1];
         Node p2 = tree[s2];
         query[s1].push_back(make_pair(p2, i));
+
         if(s1 != s2)
             query[s2].push_back(make_pair(p1, i));
     }
@@ -93,5 +95,6 @@ int main()
 
     for(int i = 0; i < m; ++i)
         cout << res[i] << endl;
+
     return 0;
 }

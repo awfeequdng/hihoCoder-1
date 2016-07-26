c57f8f5e5eb91af6a0ec02e52da1033fcf6f1288
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-26 15:54:21 +0800

    update

--- hiho16_RMQ-ST算法/demo.cpp
+++ hiho16_RMQ-ST算法/demo.cpp
@@ -5,12 +5,16 @@ int price[N];
 int res[N][20];
 int block[20];
 int n, m;
-void print();
+
 void init()
 {
     for(int i = 0; i < N; ++i)
         for(int j = 0; j < 20; ++j)
             res[i][j] = 100000;
+
+    block[0] = 1;
+    for(int i = 1; i < 20; ++i)
+        block[i] = 2 * block[i-1];
 }
 
 int getIndex(int n)
@@ -28,42 +32,39 @@ void build()
 {
     for(int i = 0; i < n; ++i)
         res[i][0] = price[i];
-    print();
-    for(int i = 0; i < n; ++i)
+    for(int j = 1; j <= getIndex(n-1); ++j)
     {
-        for(int j = 1; j <= getIndex(n-1); ++j)
+        for(int i = 0; i < n; ++i)
         {
             res[i][j] = (res[i][j-1] < res[i+block[j-1]][j-1] ? res[i][j-1] : res[i+block[j-1]][j-1]);
         }
     }
 }
 
-void print()
-{
-    for(int i = 0; i < n; ++i)
-    {
-        for(int j = 0; j <= getIndex(n-1); ++j)
-        {
-            cout << res[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
 int main()
 {
     init();
-    block[0] = 1;
-    for(int i = 1; i < 20; ++i)
-    {
-        block[i] = 2 * block[i-1];
-    }
+
     cin >> n;
     for(int i = 0; i < n; ++i)
     {
         cin >> price[i];
     }
     build();
-    print();
+
+    cin >> m;
+    int l, r;
+    while(m--)
+    {
+        cin >> l >> r;
+        if(l == r)
+        {
+            cout << price[l-1] << endl;
+            continue;
+        }
+        int d = getIndex(r - l) - 1;
+        int t = block[d];
+        cout << (res[l-1][t] < res[r-t][t] ? res[l-1][t] : res[r-t][t]) << endl;;
+    }
     return 0;
 }

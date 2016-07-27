e61608ca91c52f0d593c88acce6d9022e67d621e
zuilehongdou <15120101@bjtu.edu.cn>
2016-07-27 17:52:20 +0800

    update

--- hiho17_最近公共祖先/demo.cpp
+++ hiho17_最近公共祖先/demo.cpp
@@ -2,36 +2,78 @@
 #include <map>
 #define N 100010
 using namespace std;
-int n, m;
 
 typedef struct node
 {
     string name;
     int depth;
-    int in, out;
+    int id;
     node* father;
     vector<node*> child;
     node(string name_) : name(name_)
     {
         depth = 0;
-        in = 0;
-        out = 0;
-        father = NULL;
-    }
-    node(string name_, int depth_) : name(name_), depth(depth_)
-    {
-        in = 0;
-        out = 0;
+        id = 0;
         father = NULL;
     }
 }*Node;
 
+int n, m;
+int count = 0;
+int num = 0;
+Node root = NULL;
+map<string, Node> tree;
+map<string, int> seq;
+vector<pair<int, int>> df[19];
+int df[2 * N][19];
+
+void dfs(Node p, int d)
+{
+    p->depth = d;
+    p->id = num++;
+
+    seq[p->name] = count;
+    df[0].push_back(make_pair(p->depth, count++));
+
+    for(int i = 0; i < p->child.size(); ++i)
+    {
+        dfs(p->child[i], d+1);
+    }
+
+    if(p->father)
+        df[0].push_back(make_pair(p->father->depth, count++));
+}
+
+void st()
+{
+    int k = log2(n);
+    for(int i = 1; i <= k; ++i)
+    {
+        for(int j = 0; j + (1<<i) - 1 < 2 * n - 1; ++j)
+        {
+            int next = j + (1<<(j-1));
+            if(dp[i-1][j].first < dp[i-1][next].first)
+                dp[i].push_back(dp[i-1][j]);
+            else
+                dp[i].push_back(dp[i-1][next]);
+        }
+    }
+}
+
 int main()
 {
-    string fa, son;
+    string s1, s2;
     cin >> n;
-    while(n--)
+    for(int i = 0; i < n; ++i)
     {
-        cin >> fa >> son;
+        cin >> s1 >> s2;
+        if(tree.find(s1) == tree.end())
+            tree[s1] = new node(s1);
+        if(tree.find(s2) == tree.end())
+            tree[s2] = new node(s2);
+        tree[s1]->child.push_back(tree[s2]);
+        tree[s2]->father = tree[s1];
+        if(i == 0)
+            root = tree[s1];
     }
 }

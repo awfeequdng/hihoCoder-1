1f479e072eb91469b52fb7e151ea484861bd580b
haoyuanliu <15120101@bjtu.edu.cn>
2016-08-13 21:34:25 +0800

    update

---
+++ hiho25_SPFA算法/demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#define N 100010
+using namespace std;
+
+unsigned len[N][N];
+
+int main()
+{
+    int n, m, s, t;
+    cin >> n >> m >> s >> t;
+    memset(len, -1, sizeof(len));
+}

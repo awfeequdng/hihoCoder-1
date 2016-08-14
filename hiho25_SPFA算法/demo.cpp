0285f2e486442915ef87f88ca2956524a8dd98f0
haoyuanliu <15120101@bjtu.edu.cn>
2016-08-14 20:54:37 +0800

    add files

--- hiho25_SPFA算法/demo.cpp
+++ hiho25_SPFA算法/demo.cpp
@@ -3,7 +3,7 @@
 #define N 100010
 using namespace std;
 
-unsigned len[N][N];
+unsigned len[N][N] = {-1};
 
 int main()
 {
@@ -11,10 +11,18 @@ int main()
     cin >> n >> m >> s >> t;
     int from, to;
     unsigned length;
-    memset(len, -1, sizeof(len));
     for(int i = 1; i <= m; ++i)
     {
         cin >> from >> to >> length;
-
+        len[from][to] = len[to][from] = min(len[from][to], length);
     }
+    for(int i = 1; i <= n; ++i)
+    {
+        for(int j = 1; j <= n; ++j)
+        {
+            cout << len[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }

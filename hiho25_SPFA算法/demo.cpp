da5228fb6e5c4f4266282add108268d68b50f100
haoyuanliu <15120101@bjtu.edu.cn>
2016-08-14 08:17:19 +0800

    update

--- hiho25_SPFA算法/demo.cpp
+++ hiho25_SPFA算法/demo.cpp
@@ -9,5 +9,12 @@ int main()
 {
     int n, m, s, t;
     cin >> n >> m >> s >> t;
+    int from, to;
+    unsigned length;
     memset(len, -1, sizeof(len));
+    for(int i = 1; i <= m; ++i)
+    {
+        cin >> from >> to >> length;
+
+    }
 }

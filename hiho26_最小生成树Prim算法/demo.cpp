63d27caf391eeb99d9b347260b5a6f61d2a27df2
haoyuanliu <15120101@bjtu.edu.cn>
2016-08-17 15:55:05 +0800

    add files

--- hiho26_最小生成树Prim算法/demo.cpp
+++ hiho26_最小生成树Prim算法/demo.cpp
@@ -5,41 +5,45 @@ using namespace std;
 int main()
 {
     int n;
-    int len[N][N];
-    int temp[N][N];
-    int pre[N] = {1};
     int res = 0;
     cin >> n;
+    int len[N][N];
+    int value[N];
+    bool visit[N] = {false};
     for(int i = 1; i <= n; ++i)
     {
         for(int j = 1; j <= n; ++j)
         {
             cin >> len[i][j];
-            temp[i][j] = len[i][j];
         }
     }
-    for(int j = 1; j < n; ++j)
+    for(int i = 1; i <= n; ++i)
     {
-        int index = 2;
-        int min = len[1][2];
+        value[i] = len[1][i];
+    }
+    int k = n-1;
+    visit[1] = true;
+    while(k--)
+    {
+        int index = 0;
+        int min = 99999;
         for(int i = 2; i <= n; ++i)
         {
-            if(min > len[1][i])
+            if(!visit[i] && value[i] < min)
             {
-                min = len[1][i];
+                min = value[i];
                 index = i;
             }
         }
+        visit[index] = true;
+        res += min;
         for(int i = 1; i <= n; ++i)
         {
-            if(len[1][i] > len[1][index] + len[index][i])
+            if(!visit[i] && len[index][i] < value[i])
             {
-                len[1][i] = len[1][index] + len[index][i];
-                pre[i] = index;
+                value[i] = len[index][i];
             }
         }
-        res += temp[pre[index]][index];
-        len[1][index] = 99999;
     }
     cout << res << endl;
     return 0;

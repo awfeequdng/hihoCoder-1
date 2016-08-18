5db156132e65386e87f8f9a4b59b1dcb17e7ca1d
haoyuanliu <15120101@bjtu.edu.cn>
2016-08-18 11:10:10 +0800

    update

---
+++ hiho28_堆/demo.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#define N 100010
+using namespace std;
+
+int num[N];
+void HeapUpdate(int cur, int len)
+{
+    int pos = cur;
+    int left = cur * 2;
+    int right = cur * 2 + 1;
+    if(pos <= len/2)
+    {
+        if(left <= len && num[left] > num[pos])
+            pos = left;
+        if(right <= len && num[right] > num[pos])
+            pos = right;
+        if(pos != cur)
+        {
+            swap(num[pos], num[cur]);
+            HeapUpdate(pos, len);
+        }
+    }
+    return;
+}
+int main()
+{
+    int n;
+    char oper;
+    int len = 0;
+    cin >> n;
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> oper;
+        if(oper == 'A')
+        {
+            cin >> num[++len];
+            int l = len/2;
+            while(l)
+            {
+                HeapUpdate(l, len);
+                l = l/2;
+            }
+        }
+        else
+        {
+            cout << num[1] << endl;
+            swap(num[1], num[len--]);
+            HeapUpdate(1, len);
+        }
+    }
+    return 0;
+}
